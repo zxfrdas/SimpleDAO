03806f998480ae23905cd072f6b3a7e89207e333
zxfrdas <350491064@qq.com>
2015-06-18 11:28:03 +0800

    更新示例工程

--- Sample/src/com/simpledao/sample/TestBeanPrivate.java
+++ Sample/src/com/simpledao/sample/TestBeanPrivate.java
@@ -2,8 +2,6 @@ package com.simpledao.sample;
 
 import com.zt.simpledao.Column;
 import com.zt.simpledao.Database;
-import com.zt.simpledao.PropMethod;
-import com.zt.simpledao.PropMethodType;
 import com.zt.simpledao.SQLDataType;
 import com.zt.simpledao.Table;
 
@@ -31,64 +29,51 @@ public class TestBeanPrivate {
 	public TestBeanPrivate() {
 	}
 
-	// 字段为private时，需要生成getter&setter
-	// 需要指定该函数对应的字段名称，是get还是set操作
-	@PropMethod(name="number", type=PropMethodType.GET)
+	// 字段为private时，需要用IDE自动生成的getter&setter
 	public int getNumber() {
 		return number;
 	}
 
-	@PropMethod(name="number", type=PropMethodType.SET)
 	public void setNumber(int number) {
 		this.number = number;
 	}
 
-	@PropMethod(name="text", type=PropMethodType.GET)
 	public String getText() {
 		return text;
 	}
 
-	@PropMethod(name="text", type=PropMethodType.SET)
 	public void setText(String text) {
 		this.text = text;
 	}
 
-	@PropMethod(name="bool", type=PropMethodType.GET)
 	public boolean isBool() {
 		return bool;
 	}
 
-	@PropMethod(name="bool", type=PropMethodType.SET)
 	public void setBool(boolean bool) {
 		this.bool = bool;
 	}
 
-	@PropMethod(name="flo", type=PropMethodType.GET)
 	public double getFlo() {
 		return flo;
 	}
 
-	@PropMethod(name="flo", type=PropMethodType.SET)
 	public void setFlo(double flo) {
 		this.flo = flo;
 	}
 
-	@PropMethod(name="blob", type=PropMethodType.GET)
 	public byte[] getBlob() {
 		return blob;
 	}
 
-	@PropMethod(name="blob", type=PropMethodType.SET)
 	public void setBlob(byte[] blob) {
 		this.blob = blob;
 	}
 
-	@PropMethod(name="booltext", type=PropMethodType.GET)
 	public boolean isBooltext() {
 		return booltext;
 	}
 
-	@PropMethod(name="booltext", type=PropMethodType.SET)
 	public void setBooltext(boolean booltext) {
 		this.booltext = booltext;
 	}
